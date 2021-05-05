32b211eda47e28fea52b5b426e48d77069b96326
DariaKirova <kirova.daria@mail.ru>
2021-05-05 03:07:51 +0300

    Test
    Test

---
+++ Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.T1 = new System.Windows.Forms.TextBox();
+            this.V2 = new System.Windows.Forms.TextBox();
+            this.V1 = new System.Windows.Forms.TextBox();
+            this.V0 = new System.Windows.Forms.TextBox();
+            this.S2 = new System.Windows.Forms.TextBox();
+            this.T2 = new System.Windows.Forms.TextBox();
+            this.A = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button1.Location = new System.Drawing.Point(100, 209);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(255, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите t1:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Введите v0:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 21);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Введите v1:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 21);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Введите v2:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(270, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 25);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Результат:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(272, 62);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 21);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "a:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(272, 116);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(28, 21);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "t2:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(269, 165);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 21);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "s2:";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(201, 62);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(19, 21);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "c";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(200, 133);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(47, 21);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "m/c2";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(377, 62);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(47, 21);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "m/c2";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(201, 168);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(47, 21);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "m/c2";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(377, 114);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(19, 21);
+            this.label13.TabIndex = 13;
+            this.label13.Text = "c";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(377, 163);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(24, 21);
+            this.label14.TabIndex = 14;
+            this.label14.Text = "m";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(201, 97);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(47, 21);
+            this.label15.TabIndex = 16;
+            this.label15.Text = "m/c2";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label16.Location = new System.Drawing.Point(21, 9);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(156, 25);
+            this.label16.TabIndex = 17;
+            this.label16.Text = "Ввод значений:";
+            this.label16.Click += new System.EventHandler(this.label16_Click);
+            // 
+            // T1
+            // 
+            this.T1.Location = new System.Drawing.Point(125, 59);
+            this.T1.Name = "T1";
+            this.T1.Size = new System.Drawing.Size(70, 29);
+            this.T1.TabIndex = 21;
+            this.T1.TextChanged += new System.EventHandler(this.T1_TextChanged);
+            // 
+            // V2
+            // 
+            this.V2.Location = new System.Drawing.Point(126, 165);
+            this.V2.Name = "V2";
+            this.V2.Size = new System.Drawing.Size(68, 29);
+            this.V2.TabIndex = 22;
+            // 
+            // V1
+            // 
+            this.V1.Location = new System.Drawing.Point(125, 130);
+            this.V1.Name = "V1";
+            this.V1.Size = new System.Drawing.Size(69, 29);
+            this.V1.TabIndex = 23;
+            // 
+            // V0
+            // 
+            this.V0.Location = new System.Drawing.Point(126, 94);
+            this.V0.Name = "V0";
+            this.V0.Size = new System.Drawing.Size(69, 29);
+            this.V0.TabIndex = 24;
+            // 
+            // S2
+            // 
+            this.S2.Location = new System.Drawing.Point(300, 162);
+            this.S2.Name = "S2";
+            this.S2.ReadOnly = true;
+            this.S2.Size = new System.Drawing.Size(71, 29);
+            this.S2.TabIndex = 25;
+            // 
+            // T2
+            // 
+            this.T2.Location = new System.Drawing.Point(300, 113);
+            this.T2.Name = "T2";
+            this.T2.ReadOnly = true;
+            this.T2.Size = new System.Drawing.Size(71, 29);
+            this.T2.TabIndex = 26;
+            // 
+            // A
+            // 
+            this.A.Location = new System.Drawing.Point(300, 62);
+            this.A.Name = "A";
+            this.A.ReadOnly = true;
+            this.A.Size = new System.Drawing.Size(71, 29);
+            this.A.TabIndex = 27;
+            this.A.TextChanged += new System.EventHandler(this.A_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(436, 276);
+            this.Controls.Add(this.A);
+            this.Controls.Add(this.T2);
+            this.Controls.Add(this.S2);
+            this.Controls.Add(this.V0);
+            this.Controls.Add(this.V1);
+            this.Controls.Add(this.V2);
+            this.Controls.Add(this.T1);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Programm_1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.TextBox T1;
+        private System.Windows.Forms.TextBox V2;
+        private System.Windows.Forms.TextBox V1;
+        private System.Windows.Forms.TextBox V0;
+        private System.Windows.Forms.TextBox S2;
+        private System.Windows.Forms.TextBox T2;
+        private System.Windows.Forms.TextBox A;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Text = "Programm_1";
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool mistake = false;
+            double t1 = -1, v0 = -1, v1 = -1, v2 = -1;
+            
+            while (mistake != true)
+            {
+                t1 = Convert.ToDouble(T1.Text);
+                if  (t1 < 0){
+                    MessageBox.Show("Вы ввели отрицательное значение. Пожалуйста введите положительное число!");
+                    mistake = false;
+                    T1.Text = "";
+                }
+                else if (t1 == 0){
+                    MessageBox.Show("t1 не может равняться 0, так как в одном из уравнений оно - делимое. Пожалуйста введите положительное число!");
+                    mistake = false;
+                    T1.Text = "";
+                }
+                else if (t1 >= 10000)
+                {
+                    MessageBox.Show("Вы не можете вводить числа больше 10000. Пожалуйста введите меньшее число!");
+                    mistake = false;
+                    T1.Text = "";
+                }
+                else if ((t1>0) && (t1<10000)){
+                    t1 = Convert.ToDouble(T1.Text);
+                    mistake = true;
+                    continue;
+                }           
+                return;               
+            }
+            mistake = false;
+
+            while (mistake != true)
+            {
+                v0 = Convert.ToDouble(V0.Text);
+                if (v0 < 0){
+                    MessageBox.Show("Вы ввели отрицательное значение. Пожалуйста введите положительное число!");
+                    mistake = false;
+                    V0.Text = "";
+                }
+                else if (v0 >= 10000)
+                {
+                    MessageBox.Show("Вы не можете вводить числа больше 10000. Пожалуйста введите меньшее число!");
+                    mistake = false;
+                    V0.Text = "";
+                }
+                else {
+                    v0 = Convert.ToDouble(V0.Text);
+                    mistake = true;
+                    continue;
+                }
+                return;
+            }
+            mistake = false;
+
+            while (mistake != true)
+            {
+                v1 = Convert.ToDouble(V1.Text);
+                if (v1 < 0){
+                    MessageBox.Show("Вы ввели отрицательное значение. Пожалуйста введите положительное число!");
+                    mistake = false;
+                    V1.Text = "";
+                }
+                else if (v1 >= 10000)
+                {
+                    MessageBox.Show("Вы не можете вводить числа больше 10000. Пожалуйста введите меньшее число!");
+                    mistake = false;
+                    V1.Text = "";
+                }
+                else {
+                    v1 = Convert.ToDouble(V1.Text);
+                    mistake = true;
+                    continue;
+                }
+                return;
+            }
+            mistake = false;
+
+            while (mistake != true)
+            {
+                v2 = Convert.ToDouble(V2.Text);
+                if (v2 < 0){
+                    MessageBox.Show("Вы ввели отрицательное значение. Пожалуйста введите положительное число!");
+                    mistake = false;
+                    V2.Text = "";
+                }
+                else if (v2 >= 10000)
+                {
+                    MessageBox.Show("Вы не можете вводить числа больше 10000. Пожалуйста введите меньшее число!");
+                    mistake = false;
+                    V2.Text = "";
+                }
+                else if (v2>= 0){
+                    v2 = Convert.ToDouble(V2.Text);
+                    mistake = true;
+                    continue;
+                }
+                return;
+            }
+            mistake = false;
+
+            double a = v1 / t1;
+            double t2 = v2/ a;
+            A.Text = String.Format("{0:f2}", v1 / t1);
+            T2.Text = String.Format("{0:f2}", t2);
+            S2.Text = String.Format("{0:f2}", a *t2*t2/2);
+        }
+        private void Form1_Load(object sender, EventArgs e){   
+        }
+        private void label1_Click(object sender, EventArgs e){
+        }
+        private void label6_Click(object sender, EventArgs e){
+        }
+        private void label8_Click(object sender, EventArgs e){
+        }
+        private void label16_Click(object sender, EventArgs e){
+        }
+        private void A_TextChanged(object sender, EventArgs e){
+        }
+        private void T1_TextChanged(object sender, EventArgs e){          
+        }
+    }
+}
